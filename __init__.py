7fd7174c8516bc6770023e69ce1d091423f7521f
CCA2878 <51525400+CCA2878@users.noreply.github.com>
2021-01-28 21:03:55 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
 from json import load, dump
 from nonebot import get_bot
-from hoshino import priv
+from hoshino import Service, priv
 from hoshino.typing import NoticeSession
 from .pcrclient import pcrclient, ApiException
-from asyncio import Lock, get_event_loop
+from asyncio import Lock, get_event_loop, run_coroutine_threadsafe
 from os.path import dirname, join, exists
 from copy import deepcopy
 from traceback import format_exc
-from .safeservice import SafeService
 
-sv_help = '''[竞技场绑定 uid] 绑定竞技场排名变动推送，默认双场均启用
+sv_help = '''[竞技场绑定 uid] 绑定竞技场排名变动推送，默认双场均启用，仅排名降低时推送
 [竞技场查询 (uid)] 查询竞技场简要信息
 [停止竞技场订阅] 停止战斗竞技场排名变动推送
 [停止公主竞技场订阅] 停止公主竞技场排名变动推送
@@ -18,9 +17,7 @@ sv_help = '''[竞技场绑定 uid] 绑定竞技场排名变动推送，默认双
 [删除竞技场订阅] 删除竞技场排名变动推送绑定
 [竞技场订阅状态] 查看排名变动推送绑定状态'''
 
-query_loop = get_event_loop()
-
-sv = SafeService('竞技场推送',help_=sv_help, bundle='pcr查询', loop = query_loop)
+sv = Service('竞技场推送',help_=sv_help, bundle='pcr查询')
 
 @sv.on_fullmatch('jjc帮助', only_to_me=False)
 async def send_jjchelp(bot, ev):
@@ -45,9 +42,17 @@ binds = root['arena_bind']
 with open(join(curpath, 'account.json')) as fp:
     client = pcrclient(load(fp))
 
+query_loop = get_event_loop()
+
 qlck = Lock()
 
 async def query(id: str):
+    def syncfunc(id: str):
+        return run_coroutine_threadsafe(query(id), query_loop).result()
+    
+    if get_event_loop() != query_loop:
+        return await get_event_loop().run_in_executor(None, syncfunc, id)
+
     async with qlck:
         while client.shouldLogin:
             await client.login()
@@ -135,27 +140,28 @@ async def delete_arena_sub(bot,ev):
         uid = str(ev['user_id'])
 
 
-    if not uid in binds:
-        await bot.finish(ev, '未绑定竞技场', at_sender=True)
-        return
-
     async with lck:
+        if not uid in binds:
+            await bot.finish(ev, '未绑定竞技场', at_sender=True)
+            return
+
         binds.pop(uid)
         save_binds()
 
-    await bot.finish(ev, '删除竞技场订阅成功', at_sender=True)
+        await bot.finish(ev, '删除竞技场订阅成功', at_sender=True)
 
 @sv.on_fullmatch('竞技场订阅状态')
 async def send_arena_sub_status(bot,ev):
     global binds, lck
     uid = str(ev['user_id'])
 
-    
-    if not uid in binds:
-        await bot.send(ev,'您还未绑定竞技场', at_sender=True)
-    else:
-        info = binds[uid]
-        await bot.finish(ev,
+
+    async with lck:
+        if not uid in binds:
+            await bot.send(ev,'您还未绑定竞技场', at_sender=True)
+        else:
+            info = binds[uid]
+            await bot.finish(ev,
     f'''
     当前竞技场绑定ID：{info['id']}
     竞技场订阅：{'开启' if info['arena_on'] else '关闭'}
@@ -169,7 +175,8 @@ async def on_arena_schedule():
     
     bind_cache = {}
 
-    async with qlck:
+
+    async with lck:
         bind_cache = deepcopy(binds)
 
 
@@ -187,16 +194,16 @@ async def on_arena_schedule():
             last = cache[user]
             cache[user] = res
 
-            if res[0] != last[0] and info['arena_on']:
+            if res[0] > last[0] and info['arena_on']:
                 await bot.send_group_msg(
                     group_id = int(info['gid']),
-                    message = f'[CQ:at,qq={info["uid"]}]您的竞技场排名发生变化：{last[0]}->{res[0]}'
+                    message = f'[CQ:at,qq={info["uid"]}]\n您的竞技场排名降低：{last[0]}->{res[0]}，降低了{res[0]-last[0]}名。'
                 )
 
-            if res[1] != last[1] and info['grand_arena_on']:
+            if res[1] > last[1] and info['grand_arena_on']:
                 await bot.send_group_msg(
                     group_id = int(info['gid']),
-                    message = f'[CQ:at,qq={info["uid"]}]您的公主竞技场排名发生变化：{last[1]}->{res[1]}'
+                    message = f'[CQ:at,qq={info["uid"]}]\n您的公主竞技场排名降低：{last[1]}->{res[1]}，降低了{res[1]-last[1]}名。'
                 )
         except ApiException as e: # FIXME: 更改query实现之后可能无法抛出这个异常
             sv.logger.info(f'对{info["id"]}的检查出错\n{format_exc()}')
